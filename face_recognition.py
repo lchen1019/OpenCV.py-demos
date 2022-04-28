192c163d7920c92fdc9945dc109640a417f09afe
陈林 <1966069940@qq.com>
2022-04-28 17:51:39 +0800

    face recognition

--- face_recognition.py
+++ face_recognition.py
@@ -1,12 +1,12 @@
 import cv2
-import numpy
 
 
 def main():
-    face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
+    face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
 
-    img = cv2.imread("dog.jpg")
+    img = cv2.imread("person1.png")
+    # img = cv2.imread("person2.jpg")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -18,7 +18,7 @@ def main():
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-    cv2.imshow('img', img)
+    cv2.imshow('人脸识别', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
